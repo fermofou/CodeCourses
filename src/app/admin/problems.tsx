781df3d4b6c536a6898b5052d9eee1a37dbc9dd1
imanolgzz <imanolgzzsolis@gmail.com>
2025-04-27 10:17:23 -0600

    recopilar información para editar problema

--- src/app/admin/problems.tsx
+++ src/app/admin/problems.tsx
@@ -1,32 +1,8 @@
-// problemas.tsx
+// problemas.ts
 import { useState, useEffect } from "react";
 import { Table, Button, Modal, Form, Input, Select, Tag } from "antd";
 import { ColumnsType } from "antd/es/table";
 import { EditOutlined, PlusOutlined } from "@ant-design/icons";
-
-const initialData = [
-  {
-    key: "1",
-    title: "Sumar dos números",
-    difficulty: 2,
-    tags: ["Math", "Básico"],
-    description: "Suma dos números enteros y devuelve el resultado.",
-  },
-  {
-    key: "2",
-    title: "Camino más corto en un grafo",
-    difficulty: 4,
-    tags: ["Graphs", "Dijkstra"],
-    description: "Encuentra el camino más corto entre dos nodos en un grafo.",
-  },
-  {
-    key: "3",
-    title: "Camino más largo en un grafo",
-    difficulty: 3,
-    tags: ["Graphs", "Dijkstra"],
-    description: "Encuentra el camino más largo entre dos nodos en un grafo.",
-  },
-];
 
 interface ApiProblemType {
   problem_id: number;
@@ -34,34 +10,61 @@ interface ApiProblemType {
   difficulty: number;
   solved: boolean | null;
   tags?: string[];
-  description?: string;
+  question: string;
   // Points might be added later
 }
 
+interface ProblemData {
+  problem_id?: number; 
+  title: string;
+  difficulty: number;
+  tags: string[];
+  question: string;
+  testCases?: [
+    {
+      expectedOutput: string;
+      input: string;
+    }
+  ];
+}
 
 const Problems = () => {
   const [data, setData] = useState(undefined);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [editingProblem, setEditingProblem] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(true); 
-  const [form] = Form.useForm();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [loadingProblem, setLoadingProblem] = useState<boolean>(true);
+  const [challengeData, setChallengeData] = useState<ProblemData>({
+    title: "",
+    difficulty: 1,
+    tags: [],
+    question: "",
+    testCases: [{
+      input: "",
+      expectedOutput: "",
+    }],
+  });
+  const [zipFile, setZipFile] = useState<File | null>(null);
+  const [form] = Form.useForm(); 
+  const userID = 6;
+
   useEffect(() => {
     setLoading(true);
-    fetch("http://localhost:8080/problems?userId=6")
+    fetch(`http://localhost:8080/problems?userId=${userID}`)
       .then((response) => response.json())
       .then((data: ApiProblemType[]) => {
         // Map the API data to match our table structure
         const formattedProblems = data.map((problem) => {
           return {
             key: problem.problem_id,
-            status: problem.solved, // Convert null to false
             title: problem.title,
             difficulty: problem.difficulty,
             tags: problem.tags || ["coding"], // Default tag if not provided
+            question: problem.question,
+
           };
         });
-        console.log("formattedProblems", formattedProblems);
         setData(formattedProblems);
         setLoading(false);
       })
@@ -71,44 +74,65 @@ const Problems = () => {
       });
   }, []);
 
+  const fetchProblem = async (problemId: number) => {
+    try {
+      const response = await fetch(`http://localhost:8080/challenge?probID=${problemId}&userID=${userID}`);
+      const data = await response.json(); 
+      data.testCases = JSON.parse(data.tests);
+      setChallengeData({
+        title: data.title,
+        difficulty: data.difficulty,
+        tags: data.tags,
+        question: data.question,
+        testCases: data.testCases.map((testCase: any) => ({
+          input: testCase.input,
+          expectedOutput: testCase.expectedOutput,
+        })),
+      }) 
+      return data;
+    } catch (error) {
+      console.error("Error fetching problem:", error);
+    }
+  }
+
   const showAddModal = () => {
     setIsEditing(false);
     form.resetFields();
     setIsModalVisible(true);
   };
 
-  const showEditModal = (record: any) => {
+  const showEditModal = async (record: any) => {
     setIsEditing(true);
     setEditingProblem(record);
     form.setFieldsValue(record);
+    setLoadingProblem(true); 
     setIsModalVisible(true);
+    await fetchProblem(record.key);
+    setLoadingProblem(false);
   };
 
-  const handleOk = async () => {
-    await form.validateFields().then(async values => {
-      if (isEditing) {
-      setData(prev =>
-        prev.map(item =>
-        item.key === editingProblem.key ? { ...editingProblem, ...values } : item
-        )
-      );
-      } else {
+
+  const handleEditProblem = async () => {
+    
+  }
+
+  const handleUploadProblem = async () => {
+      const values = challengeData; 
       const newProblem = { ...values, key: Date.now().toString() };
       setData(prev => [...prev, newProblem]);
       
-      
       const SampleTests = newProblem.testCases != undefined ? JSON.stringify(
-        newProblem.testCases.map((testCase: any) => ({ input: testCase.testInput, expectedOutput: testCase.testOutput,
+        newProblem.testCases.map((testCase: any) => ({ input: testCase.input, expectedOutput: testCase.expectedOutput,
         }))
       ) : "[]";
 
       const data = {
-        Title: newProblem.title,
-        Difficulty: newProblem.difficulty,
-        TimeLimit: 5,
-        MemoryLimit: 256,
-        SampleTests: SampleTests,
-        Question: newProblem.description,
+        title: newProblem.title,
+        difficulty: newProblem.difficulty,
+        timeLimit: 5,
+        memoryLimit: 256,
+        sampleTests: SampleTests,
+        question: newProblem.question,
       };
       console.log("data", data);
       console.log("values", newProblem);
@@ -126,17 +150,35 @@ const Problems = () => {
       } catch (error) {
         console.error("Error uploading problem statement:", error);
       }
-      }
-      setIsModalVisible(false);
-      form.resetFields();
+  }
+
+  const handleClose = () => {
+    setIsModalVisible(false);
+    setChallengeData({
+      title: "",
+      difficulty: 1,
+      tags: [],
+      question: "",
+      testCases: [{
+        input: "",
+        expectedOutput: "",
+      }],
     });
+  }
+
+  const handleOk = async () => {
+      if (isEditing) {
+        await handleEditProblem();
+      } else {
+        await handleUploadProblem();
+      }
+      handleClose();
   };
 
   const handleCancel = () => {
-    setIsModalVisible(false);
-    form.resetFields();
+    handleClose();
   };
-
+  
   const columns: ColumnsType<any> = [
     {
       title: "Título",
@@ -155,7 +197,7 @@ const Problems = () => {
           4: "★★★★☆",
           5: "★★★★★",
         };
-        return <Tag> {stars[difficulty]}</Tag>;
+        return <Tag>{stars[difficulty]}</Tag>;
       },
     },
     {
@@ -175,7 +217,7 @@ const Problems = () => {
       key: "action",
       render: (_: any, record: any) => (
         <div className="flex gap-3">
-          <Button icon={<EditOutlined />} onClick={() => showEditModal(record)}>
+          <Button icon={<EditOutlined />} onClick={() => {showEditModal(record);}}>
             Editar
           </Button>
           <Button danger>
@@ -198,7 +240,6 @@ const Problems = () => {
         />
       </div>
       <Table columns={columns} dataSource={data} />
-
       <Modal
         title={isEditing ? "Editar Problema" : "Agregar Problema"}
         visible={isModalVisible}
@@ -207,34 +248,19 @@ const Problems = () => {
         okText="Guardar"
         cancelText="Cancelar"
       >
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={values => {
-            console.log("Form values:", values);
-            if (!values.zipFile) {
-              return;
-            }
-            handleOk();
-          }}
-        >
-        <Form.Item
-          label="Título"
-          style={{ marginBottom: "1rem" }}
-          name="title"
-          rules={[{ required: true, message: "Por favor ingresa el título" }]}
-        >
-          <Input id="problem-title" />
-        </Form.Item>
-
-        <Form.Item
-          style={{ marginBottom: "1rem" }}
-          label="Dificultad"
-          name="difficulty"
-          rules={[{ required: true, message: "Selecciona una dificultad" }]}
-        >
+        {loadingProblem && <p>Cargando problema...</p>}
+        {!loadingProblem && ( 
+        <> 
+        <div style={{marginBottom: "1rem", display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+          Título
+          <Input value={challengeData.title} onChange={(e : any) => {setChallengeData({ ...challengeData, title: e.target.value })}} placeholder="Título del problema" />
+        </div>
+        <div style={{marginBottom: "1rem", display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+          Dificultad 
           <Select
             placeholder="Selecciona dificultad"
+            value={challengeData.difficulty}
+            onChange={(value) => {setChallengeData({ ...challengeData, difficulty: value })}} 
             options={[
               { value: 1, label: "★☆☆☆☆ Muy Fácil" },
               { value: 2, label: "★★☆☆☆ Fácil" },
@@ -243,101 +269,122 @@ const Problems = () => {
               { value: 5, label: "★★★★★ Muy Difícil" },
             ]}
           />
-        </Form.Item>
-
-        <Form.Item
-          label="Tags"
-          name="tags"
-          style={{ marginBottom: "1rem" }}
-          rules={[{ required: true, message: "Agrega al menos un tag" }]}
-        >
-        <Select
-          mode="tags"
-          style={{ width: "100%" }}
-          placeholder="Tags"
-          options={[
-            { value: "Math", label: "Math" },
-            { value: "DP", label: "DP" },
-            { value: "Greedy", label: "Greedy" },
-            { value: "Graphs", label: "Graphs" },
-            { value: "Binary Search", label: "Binary Search" },
-            { value: "Brute Force", label: "Brute Force" },
-            { value: "Combinatorics", label: "Combinatorics" },
-            { value: "Data Structures", label: "Data Structures" },
-            { value: "Divide and Conquer", label: "Divide and Conquer" },
-            { value: "Geometry", label: "Geometry" },
-            { value: "Implementation", label: "Implementation" },
-            { value: "Number Theory", label: "Number Theory" },
-            { value: "Shortest Paths", label: "Shortest Paths" },
-            { value: "Sorting", label: "Sorting" },
-            { value: "Strings", label: "Strings" },
-            { value: "Trees", label: "Trees" },
-            { value: "Two Pointers", label: "Two Pointers" },
-          ]}
-        />
-        </Form.Item>
-          <Form.Item
-            label="Descripción"
-            name="description"
-            rules={[{ required: true, message: "Por favor ingresa la descripción" }]}
-          >
-            <Input.TextArea rows={7} placeholder="Descripción del problema" />
-          </Form.Item>
-          <Form.List name="testCases">
-            {(fields, { add, remove }) => (
-            <>
-              {fields.map(({ key, name, fieldKey, ...restField }) => (
-                <div key={key} className="mb-4 border p-3 rounded-md">
-              <Form.Item
-                {...restField}
-                label="Input de Prueba"
-                name={[name, "testInput"]}
-                fieldKey={[fieldKey, "testInput"]}
-                rules={[{ required: true, message: "Por favor ingresa un input de prueba" }]}
-              >
-                <Input.TextArea rows={2} placeholder="Input de prueba" />
-              </Form.Item>
-
-              <Form.Item
-                {...restField}
-                label="Output de Prueba"
-                name={[name, "testOutput"]}
-                fieldKey={[fieldKey, "testOutput"]}
-                rules={[{ required: true, message: "Por favor ingresa el output esperado" }]}
-              >
-                <Input.TextArea rows={2} placeholder="Output esperado" />
-              </Form.Item>
-
-              <Button danger onClick={() => remove(name)}>
-                Eliminar Testcase de Ejemplo 
-              </Button>
-                </div>
-              ))}
-              <Button style={{marginBottom: "1rem"}} type="dashed" onClick={() => add()} block>
-                Agregar Testcase de Ejemplo 
-              </Button>
-            </>
-            )}
-          </Form.List>
-          {!isEditing && (
-          <>
-            <Form.Item
-              label="Subir Testcases (ZIP)"
-              name="zipFile"
-              rules={[{ required: true, message: "Por favor sube un archivo ZIP con los testcases" }]}
-              valuePropName="zipFile"
-              getValueFromEvent={(e) => e} // importante para capturar el evento original
-            >
-              <input type="file" accept=".zip" />
-            </Form.Item>
-            <div className="text-sm text-gray-600 italic mt-2">
-              Asegúrate de que el archivo ZIP contenga los testcases en el formato: 
-              <br />
-              <strong>1.in, 1.out, 2.in, 2.out, ...</strong>
+        </div>
+        <div style={{marginBottom: "1rem", display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+          Tags 
+          <Select
+            mode="tags"
+            style={{ width: "100%" }}
+            placeholder="Tags"
+            value={challengeData.tags}
+            onChange={(value) => {setChallengeData({ ...challengeData, tags: value })}}
+            options={[
+              { value: "Math", label: "Math" },
+              { value: "DP", label: "DP" },
+              { value: "Greedy", label: "Greedy" },
+              { value: "Graphs", label: "Graphs" },
+              { value: "Binary Search", label: "Binary Search" },
+              { value: "Brute Force", label: "Brute Force" },
+              { value: "Combinatorics", label: "Combinatorics" },
+              { value: "Data Structures", label: "Data Structures" },
+              { value: "Divide and Conquer", label: "Divide and Conquer" },
+              { value: "Geometry", label: "Geometry" },
+              { value: "Implementation", label: "Implementation" },
+              { value: "Number Theory", label: "Number Theory" },
+              { value: "Shortest Paths", label: "Shortest Paths" },
+              { value: "Sorting", label: "Sorting" },
+              { value: "Strings", label: "Strings" },
+              { value: "Trees", label: "Trees" },
+              { value: "Two Pointers", label: "Two Pointers" },
+            ]}
+          />
+        </div>
+        <div style={{marginBottom: "1rem", display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+          Descripción
+          <Input.TextArea 
+            value={challengeData.question} 
+            onChange={(e: any) => {setChallengeData({ ...challengeData, question: e.target.value })}} 
+            rows={6} 
+            placeholder="Descripción del problema" 
+          />
+        </div> 
+        <div>
+          {(challengeData?.testCases || []).map((testCase, index) => (
+            <div key={index} className="mb-4 border p-3 rounded-md">
+            <div style={{ marginBottom: "1rem", display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+              <label>Input de Prueba</label>
+              <Input.TextArea
+              rows={2}
+              placeholder="Input de prueba"
+              value={testCase.input}
+              onChange={(e : any) => {
+                const updatedTestCases = [...(challengeData?.testCases || [])];
+                updatedTestCases[index] = {
+                ...updatedTestCases[index],
+                input: e.target.value,
+                };
+                setChallengeData({ ...challengeData, testCases: updatedTestCases });
+              }}
+              />
             </div>
-          </>
+
+            <div style={{ marginBottom: "1rem", display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+              <label>Output de Prueba</label>
+              <Input.TextArea
+              rows={2}
+              placeholder="Output esperado"
+              value={testCase.expectedOutput}
+              onChange={(e : any) => {
+                const updatedTestCases = [...(challengeData?.testCases || [])];
+                updatedTestCases[index] = {
+                ...updatedTestCases[index],
+                expectedOutput: e.target.value,
+                };
+                setChallengeData({ ...challengeData, testCases: updatedTestCases });
+              }}
+              />
+            </div>
+
+            <Button danger onClick={() => {
+              const updatedTestCases = [...(challengeData?.testCases || [])];
+              updatedTestCases.splice(index, 1);
+              setChallengeData({ ...challengeData, testCases: updatedTestCases });
+            }}>
+              Eliminar Testcase de Ejemplo
+            </Button>
+            </div>
+          ))}
+          <Button style={{ marginBottom: "1rem" }} type="dashed" onClick={() => {
+            const updatedTestCases = [...(challengeData?.testCases || []), { input: "", expectedOutput: "" }];
+            setChallengeData({ ...challengeData, testCases: updatedTestCases });
+          }} block>
+            Agregar Testcase de Ejemplo
+          </Button>
+        </div>
+        {!isEditing && (
+        <>
+          <input
+            name="file"
+            type="file"
+            accept=".zip"
+            onChange={(e) => setZipFile(e.target.files?.[0] || null)}
+          />
+          {zipFile && (
+            <div className="text-sm text-gray-600 italic mt-2">
+            Archivo seleccionado: {zipFile.name}
+            </div>
           )}
-        </Form>
+          <div className="text-sm text-gray-600 italic mt-2">
+            Asegúrate de que el archivo ZIP contenga los testcases en el formato: 
+            <br />
+            <strong>1.in, 1.out, 2.in, 2.out, ...</strong>
+          </div>
+        </>
+        )}
+        </>
+        )}
+
+
       </Modal>
 
     </div>
