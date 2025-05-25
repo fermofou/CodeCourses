ddffa28aef06c5a21f9c5cab5a5e4cc2bd0a1f00
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2025-05-25 13:55:45 -0600

    Correct import for navbar

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 import Leftbar from "./leftbar";
 import Problems from "./problems";
 import Users from "./users";
