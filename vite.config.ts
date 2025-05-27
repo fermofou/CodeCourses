2411be327c21b522d9e42a120afcfb4022820528
fermofou <fermofou@gmail.com>
2025-05-26 19:11:09 -0600

    update proxys

--- vite.config.ts
+++ vite.config.ts
@@ -14,12 +14,12 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api/leaderboard": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/leaderboard/, "/leaderboard"),
       },
       "/claim": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
       },
       "/execute": {
@@ -35,63 +35,66 @@ export default defineConfig({
         changeOrigin: true,
       },
       "/api/challenge": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/challenge/, "/challenge"),
       },
       "/user": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
       },
       "/api/problems": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/problems/, "/problems"),
       },
       "/api/rewards": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/rewards/, "/rewards"),
       },
       "api/challenge": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
       },
       "/api/badges": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/badges/, "/badges"),
       },
       "/api/admin/users": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/admin\/users/, "/admin/users"),
       },
       "/api/admin/users/:id": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/api\/admin\/users\/(.*)/, "/admin/updateUser/$1"),
       },
       "/admin/user/:id/badges": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/admin\/user\/(.*)\/badges/, "/admin/user/$1/badges"),
       },
       "/admin/user/:id/updateBadges": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) =>
-          path.replace(/^\/admin\/user\/(.*)\/badges/, "/admin/user/$1/updateBadges"),
+          path.replace(
+            /^\/admin\/user\/(.*)\/badges/,
+            "/admin/user/$1/updateBadges"
+          ),
       },
       "/api/admin/claims": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/admin/, "/admin"),
       },
       "/api/myRewards": {
-        target: "http://localhost:8080",
+        target: "http://142.93.10.227:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/myRewards/, "/myRewards"),
       },
